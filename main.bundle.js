cdb64392306c91ed5f8016da1298b1942261941b
t0mdd <tomdd@protonmail.com>
2022-07-17 15:23:57 +0100

    Add webpack structure and page initialisation module
    The module clears the contents div then fills with the contents of the
    home page. This is to allow for a tabbed layout so that other tabs can
    instead fill the contents div.

---
+++ main.bundle.js
@@ -0,0 +1,19 @@
+(self["webpackChunkrestaurantpage"] = self["webpackChunkrestaurantpage"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log('CUdNT');
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnRwYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdDVWROVCcpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
